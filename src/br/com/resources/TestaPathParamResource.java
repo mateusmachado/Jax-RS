2967cd4e8f173ed60a00e015b92a2faf71090ecd
Mateus <mmachado.csp@gmail.com>
2015-05-27 01:39:12 -0300

    Tests

---
+++ src/br/com/resources/TestaPathParamResource.java
@@ -0,0 +1,19 @@
+package br.com.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/path-param")
+public class TestaPathParamResource {
+
+	@GET
+	@Path("/{p1}/{p2}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String pathParam(@PathParam("p1") String p1,
+			@PathParam("p2") String p2) {
+		return "P1 = " + p1 + " P2 = " + p2;
+	}
+}
