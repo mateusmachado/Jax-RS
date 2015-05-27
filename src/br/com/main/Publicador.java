450759e51b9e2d2c4b825dcf2f7fa05d84fd9f88
Mateus <mmachado.csp@gmail.com>
2015-05-27 00:04:54 -0300

    RESTful

---
+++ src/br/com/main/Publicador.java
@@ -0,0 +1,15 @@
+package br.com.main;
+
+import java.io.IOException;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+public class Publicador {
+	public static void main(String[] args) throws IllegalArgumentException,
+			IOException {
+		HttpServer server = HttpServerFactory.create("http://localhost:8080/");
+		server.start();
+	}
+
+}
